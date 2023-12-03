4117279944d43e959668a2b8a2fc25c52abb9262
P D <dnaspider@live.com>
2023-12-03 11:59:15 -0500

    01b

--- Project01/main.cpp
+++ Project01/main.cpp
@@ -5,12 +5,6 @@
 using namespace std;
 
 int main() {
-	//char cr = '9';
-	//cout << (int)cr;
-
-	//Sleep(2222);
-	//return 0;
-
 	auto db = "C:\\Users\\dnasp\\Desktop\\ae\\01\\input.txt";
 	wifstream f(db);
 	if (!f) {
@@ -23,19 +17,116 @@ int main() {
 		wchar_t slot1{}, slot2{};
 		wstring slot3{};
 		for (size_t i = 0; i <= cell.length(); ++i) {
-			if(cell[i] >= 48 && cell[i] <= 57 && slot1 == 0) {
-				slot1 = cell[i];
-				//continue;
+			switch (cell[i]) {
+			case '0':
+			case '1':
+			case '2':
+			case '3':
+			case '4':
+			case '5':
+			case '6':
+			case '7':
+			case '8':
+			case '9':
+			{
+				if (slot1 == 0)
+					slot1 = cell[i];
+				else
+					slot2 = cell[i];
 			}
-			if (cell[i] >= 48 && cell[i] <= 57)
-				slot2 = cell[i];
+			break;
+			case 'o':
+			{
+				if (cell.substr(i, 3) == L"one") {
+					if (slot1 == 0)
+						slot1 = '1';
+					else
+						slot2 = '1';
+				}
+			}
+			break;
+			case 't':
+			{
+				if (cell.substr(i, 3) == L"two") {
+					if (slot1 == 0)
+						slot1 = '2';
+					else
+						slot2 = '2';
+				}
+				if (cell.substr(i, 5) == L"three") {
+					if (slot1 == 0)
+						slot1 = '3';
+					else
+						slot2 = '3';
+				}
+
+			}
+			break;
+			case 'f':
+			{
+				if (cell.substr(i, 4) == L"four") {
+					if (slot1 == 0)
+						slot1 = '4';
+					else
+						slot2 = '4';
+				}
+				if (cell.substr(i, 4) == L"five") {
+					if (slot1 == 0)
+						slot1 = '5';
+					else
+						slot2 = '5';
+				}
+			}
+			break;
+			case 's':
+			{
+				if (cell.substr(i, 3) == L"six") {
+					if (slot1 == 0)
+						slot1 = '6';
+					else
+						slot2 = '6';
+				}
+				if (cell.substr(i, 5) == L"seven") {
+					if (slot1 == 0)
+						slot1 = '7';
+					else
+						slot2 = '7';
+				}
+			}
+			break;
+			case 'e':
+			{
+				if (cell.substr(i, 5) == L"eight") {
+					if (slot1 == 0)
+						slot1 = '8';
+					else
+						slot2 = '8';
+				}
+			}
+			break;
+			case 'n':
+			{
+				if (cell.substr(i, 4) == L"nine") {
+					if (slot1 == 0)
+						slot1 = '9';
+					else
+						slot2 = '9';
+				}
+			}
+			break;
+
+			default:
+				break;
+			}
+
 			if (i == cell.length() - 1) {
-				wcout << cell << " " << slot1 << " " << slot2;
-				slot3 += slot1; slot3 += slot2;
-				wcout << " " << slot3;
-				sum += stoi(slot3); //- (48*2);
-				//sum += (slot1 + slot2); //- (48*2);
-				wcout << " " << sum << endl;
+				//wcout << cell << " " << slot1 << " " << slot2;
+				slot3 += slot1;
+				if (slot2 == 0) slot2 = slot1;
+				slot3 += slot2;
+				
+				sum += stoi(slot3);
+				//wcout << " " << slot3 << " " << sum << endl;
 			}
 		}
 
